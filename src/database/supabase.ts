056dc4a7e4b957a21d2a8721ad6fadb79db8f586
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-06-30 00:19:30 -0300

    Supress supabase warning

--- src/database/supabase.ts
+++ src/database/supabase.ts
@@ -2,7 +2,12 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
 	process.env.NEXT_PUBLIC_SUPABASE_URL!,
-	process.env.NEXT_PUBLIC_SUPABASE_KEY!
+	process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+	{
+		auth: {
+			persistSession: false
+		}
+	}
 )
 
 export default supabase;
