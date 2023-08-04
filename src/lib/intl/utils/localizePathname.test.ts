dc699dcf25201fe4d312e213b81973ec89038977
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-04 00:46:36 -0300

    Added tests to pipeline

--- src/lib/intl/utils/localizePathname.test.ts
+++ src/lib/intl/utils/localizePathname.test.ts
@@ -3,7 +3,7 @@ import localizePathname from './localizePathname';
 describe('localizePathname', () => {
 	const locale = 'en-US';
 	const path = 'test';
-	const result = '\\en-US\\test';
+	const result = '/en-US/test';
 
 	test('with slashes', () => {
 		expect(localizePathname(`/${locale}`, `/${path}`)).toEqual(result);
