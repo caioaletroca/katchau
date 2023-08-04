c3334880db90336584f162389e3e67fbda5b3ad1
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-04 00:01:58 -0300

    Here we go

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -22,7 +22,7 @@ const handler = NextAuth({
 	},
 	callbacks: {
 		session: async ({ session, token }) => {
-			if (session?.user) {
+			if (session?.user && token.sub) {
 				session.user.id = token.sub;
 			}
 			return session;
