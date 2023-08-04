c13290224946bbc3ec51e1ea53b3d8ed61e61c6b
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-03 21:48:53 -0300

    Loading skeleton

--- src/app/[locale]/profile/Info.tsx
+++ src/app/[locale]/profile/Info.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { useUserProfileImage } from '@/api/profileImage';
 import { useUser } from '@/api/users';
-import { Typography } from '@mui/material';
+import Avatar from '@/components/Avatar';
+import { Skeleton, Typography } from '@mui/material';
 import { useIntl } from 'react-intl';
 
 type InfoBlockProps = {
@@ -16,40 +18,55 @@ const InfoBlock = ({ label, value = 0 }: InfoBlockProps) => (
 	</div>
 );
 
+const LoadingInfo = () => (
+	<div className="flex flex-col p-4">
+		<div className="mb-2 flex flex-row justify-between">
+			<Skeleton variant="circular" width={56} height={56} />
+			<Skeleton variant="rounded" width="25%" height={56} />
+			<Skeleton variant="rounded" width="25%" height={56} />
+			<Skeleton variant="rounded" width="25%" height={56} />
+		</div>
+	</div>
+);
+
 type InfoProps = {
 	user_id: string;
 };
 
 export default function Info({ user_id }: InfoProps) {
 	const intl = useIntl();
-	const { data: user } = useUser({ user_id });
+	const { data: user, isLoading: userLoading } = useUser({ user_id });
+	const { data: profileImage, isLoading: profileImageLoading } =
+		useUserProfileImage({ user_id });
+
+	if (userLoading || profileImageLoading) {
+		return <LoadingInfo />;
+	}
 
 	return (
 		<div className="flex flex-col p-4">
-			<div className="flex flex-row justify-between">
-				<div />
-				<div className="flex flex-row gap-4">
-					<InfoBlock
-						label={intl.formatMessage({
-							id: 'profile.postsLabel',
-							defaultMessage: 'Posts',
-						})}
-					/>
-					<InfoBlock
-						label={intl.formatMessage({
-							id: 'profile.followersLabel',
-							defaultMessage: 'Followers',
-						})}
-					/>
-					<InfoBlock
-						label={intl.formatMessage({
-							id: 'profile.followingLabel',
-							defaultMessage: 'Following',
-						})}
-					/>
-				</div>
+			<div className="mb-2 flex flex-row justify-between">
+				<Avatar name={user?.name!} url={profileImage?.url!} />
+				<InfoBlock
+					label={intl.formatMessage({
+						id: 'profile.postsLabel',
+						defaultMessage: 'Posts',
+					})}
+				/>
+				<InfoBlock
+					label={intl.formatMessage({
+						id: 'profile.followersLabel',
+						defaultMessage: 'Followers',
+					})}
+				/>
+				<InfoBlock
+					label={intl.formatMessage({
+						id: 'profile.followingLabel',
+						defaultMessage: 'Following',
+					})}
+				/>
 			</div>
-			<Typography>{user?.name}</Typography>
+			<Typography variant="body2">{user?.name}</Typography>
 		</div>
 	);
 }
--- src/app/[locale]/users/[user_id]/page.tsx
+++ src/app/[locale]/users/[user_id]/page.tsx
@@ -5,6 +5,7 @@ import PageMobileHeader from '@/components/Page/PageMobileHeader';
 import { useRouter } from '@/lib/intl/client';
 import { useParams } from 'next/navigation';
 import Content from '../../profile/Content';
+import Info from '../../profile/Info';
 
 export default function UserPage() {
 	const router = useRouter();
@@ -15,6 +16,7 @@ export default function UserPage() {
 	return (
 		<PageMobile>
 			<PageMobileHeader onBackClick={handleBack} />
+			<Info user_id={user_id} />
 			<Content user_id={user_id} />
 		</PageMobile>
 	);
