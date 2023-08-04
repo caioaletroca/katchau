33d220de30b2abf5731177c621cba6c3b3cd4856
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-03 22:25:27 -0300

    Added profile image seed

--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -1,9 +1,14 @@
-
 import { faker } from '@faker-js/faker';
-import { Post, PostImage, PrismaClient, User } from '@prisma/client';
+import {
+	Post,
+	PostImage,
+	PrismaClient,
+	ProfileImage,
+	User,
+} from '@prisma/client';
 import dotenv from 'dotenv';
-dotenv.config({ path: ".env.local" });
 import supabase from '../../src/database/supabase';
+dotenv.config({ path: '.env.local' });
 
 const prisma = new PrismaClient();
 
@@ -14,15 +19,16 @@ const MAX_IMAGES = 30;
 async function generateUsers(prisma: PrismaClient, n: number = 1) {
 	let users: User[] = [];
 
- 	for(let i = 0; i < n; i++) {
+	for (let i = 0; i < n; i++) {
 		const firstName = faker.person.firstName();
 		const lastName = faker.person.lastName();
 		const name = faker.person.fullName({ firstName, lastName });
 		const email = faker.internet.email({ firstName, lastName });
 		const user = await prisma.user.create({
 			data: {
-				name, email
-			}
+				name,
+				email,
+			},
 		});
 		users.push(user);
 	}
@@ -32,16 +38,20 @@ async function generateUsers(prisma: PrismaClient, n: number = 1) {
 	return users;
 }
 
-async function generatePosts(prisma: PrismaClient, users: User[], n: number = 1) {
+async function generatePosts(
+	prisma: PrismaClient,
+	users: User[],
+	n: number = 1
+) {
 	let posts: Post[] = [];
-	for(let i = 0; i < n; i++) {
+	for (let i = 0; i < n; i++) {
 		const index = Math.floor(Math.random() * (users.length - 1));
 		const description = faker.lorem.paragraph();
 		const post = await prisma.post.create({
 			data: {
 				user_id: users[index].id,
-				description
-			}
+				description,
+			},
 		});
 		posts.push(post);
 	}
@@ -51,26 +61,67 @@ async function generatePosts(prisma: PrismaClient, users: User[], n: number = 1)
 	return posts;
 }
 
-async function generatePostImages(prisma: PrismaClient, users: User[], posts: Post[], n: number = 1) {
+async function generateProfileImages(prisma: PrismaClient, users: User[]) {
+	let profileImages: ProfileImage[] = [];
+
+	for (let i = 0; i < users.length; i++) {
+		const url = faker.image.avatar();
+		const file = await fetch(url).then((res) => res.blob());
+
+		const profileImage = await prisma.profileImage.create({
+			data: {
+				user_id: users[i].id,
+			},
+		});
+
+		const filePath = `/${users[0].id}/${profileImage.id}.jpeg`;
+		const fileResponse = await supabase.storage
+			.from('profiles')
+			.upload(filePath, file);
+
+		const newProfileImage = await prisma.profileImage.update({
+			where: {
+				id: profileImage.id,
+			},
+			data: {
+				url: fileResponse.data?.path,
+			},
+		});
+		profileImages.push(newProfileImage);
+	}
+
+	console.log(`${profileImages.length} profile images created`);
+
+	return profileImages;
+}
+
+async function generatePostImages(
+	prisma: PrismaClient,
+	users: User[],
+	posts: Post[],
+	n: number = 1
+) {
 	let images: PostImage[] = [];
 
-	if(posts.length !== n) {
+	if (posts.length !== n) {
 		throw Error('Posts number different than images number');
 	}
 
-	for(let i = 0; i < n; i++) {
+	for (let i = 0; i < n; i++) {
 		const url = faker.image.url({ width: 600, height: 600 });
-		const file = await fetch(url).then(res => res.blob());
-		const user = users.find(user => user.id === posts[i].user_id);
+		const file = await fetch(url).then((res) => res.blob());
+		const user = users.find((user) => user.id === posts[i].user_id);
 
 		const filePath = `/${user?.id}/${posts[i].id}.jpeg`;
-		const fileResponse = await supabase.storage.from('posts').upload(filePath, file);
-		
+		const fileResponse = await supabase.storage
+			.from('posts')
+			.upload(filePath, file);
+
 		const image = await prisma.postImage.create({
 			data: {
 				post_id: posts[i].id,
-				url: fileResponse.data?.path!
-			}
+				url: fileResponse.data?.path!,
+			},
 		});
 		images.push(image);
 	}
@@ -82,29 +133,31 @@ async function generatePostImages(prisma: PrismaClient, users: User[], posts: Po
 
 export async function main() {
 	const users = await generateUsers(prisma, MAX_USERS);
+	const profileImages = await generateProfileImages(prisma, users);
 	const posts = await generatePosts(prisma, users, MAX_POSTS);
 	const images = await generatePostImages(prisma, users, posts, MAX_IMAGES);
 
 	// Special users
 	const specialUsers = [
 		{
-			name: "Mark Empty",
-			email: "mark.empty@gmail.com",
+			name: 'Mark Empty',
+			email: 'mark.empty@gmail.com',
 		},
 		{
 			name: 'Sarah Troti',
-			email: "sarah.troti@gmail.com",
-		}
+			email: 'sarah.troti@gmail.com',
+		},
 	];
 
 	await prisma.user.create({
-		data: specialUsers[0]
+		data: specialUsers[0],
 	});
 	const sarah = await prisma.user.create({
-		data: specialUsers[1]
+		data: specialUsers[1],
 	});
 
 	const sarahPosts = await generatePosts(prisma, [sarah], 20);
+	const sarahProfileImage = await generateProfileImages(prisma, [sarah]);
 	const sarahImages = await generatePostImages(prisma, [sarah], sarahPosts, 20);
 }
 
