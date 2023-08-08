8a63a372a0f41c917debf7738ee4e7e105735bbc
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-08 00:36:25 -0300

    Better search

--- src/app/[locale]/search/Content.tsx
+++ src/app/[locale]/search/Content.tsx
@@ -4,13 +4,16 @@ import { useUserSearch } from '@/api/users';
 import Icon from '@/components/Icon';
 import UserListItem from '@/components/UserListItem';
 import { useDebounce } from '@/hooks/useDebounce';
+import { useRouter, useUnlocalizedPathname } from '@/lib/intl/client';
 import {
 	CircularProgress,
+	IconButton,
 	InputAdornment,
 	List,
 	TextField,
 	Typography,
 } from '@mui/material';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 import { useIntl } from 'react-intl';
 
@@ -37,11 +40,51 @@ const LoadingScreen = () => (
 
 export default function Content() {
 	const intl = useIntl();
+	const router = useRouter();
+	const pathname = useUnlocalizedPathname();
+	const searchParams = useSearchParams();
 	const [search, setSearch] = React.useState('');
 	const { data: users, isLoading, trigger } = useUserSearch();
 
+	React.useEffect(() => {
+		// Get current query values
+		const current = new URLSearchParams(Array.from(searchParams.entries()));
+
+		// Set local value
+		const searchString = current.get('q');
+
+		// Do nothing if empty
+		if (searchString === '') {
+			return;
+		}
+
+		setSearch(searchString!);
+
+		// Trigger request
+		trigger(`/users?name=${search}`);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	useDebounce(
 		async () => {
+			// Get current query values
+			const current = new URLSearchParams(Array.from(searchParams.entries()));
+
+			// Avoid setting if empty
+			if (search === '') {
+				current.delete('q');
+			} else {
+				current.set('q', search);
+			}
+
+			const queryString = current.toString();
+			const query = queryString ? `?${queryString}` : '';
+
+			// Re-route front-end
+			router.push(`${pathname}${query}`);
+
+			// Send search request
 			trigger(`/users?name=${search}`);
 		},
 		300,
@@ -63,16 +106,21 @@ export default function Content() {
 							<Icon name="search" />
 						</InputAdornment>
 					),
+					endAdornment: search !== '' && (
+						<InputAdornment position="end">
+							<IconButton onClick={() => setSearch('')}>
+								<Icon name="close" />
+							</IconButton>
+						</InputAdornment>
+					),
 				}}
 				onChange={(e) => setSearch(e.target.value)}
 				size="small"
 				fullWidth
 			/>
-			{isLoading ? (
-				<LoadingScreen />
-			) : !isLoading && users?.length === 0 ? (
-				<EmptyScreen />
-			) : (
+			{isLoading && <LoadingScreen />}
+			{!isLoading && users?.length === 0 && search !== '' && <EmptyScreen />}
+			{!isLoading && users?.length !== 0 && search !== '' && (
 				<List>
 					{users?.map((user) => (
 						<UserListItem key={user.id} user={user} />
