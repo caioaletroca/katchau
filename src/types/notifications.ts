1b9fe0db48bfb6ec246af450526f45a40d05863f
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-11 00:24:52 -0300

    Notification typings

---
+++ src/types/notifications.ts
@@ -0,0 +1,17 @@
+import { Prisma } from '@prisma/client';
+
+export type NotificationEvent = 'liked_post' | 'liked_comment' | 'followed';
+
+const notificationWithActor = Prisma.validator<Prisma.NotificationArgs>()({
+	include: {
+		actor: {
+			include: {
+				profile_picture: true,
+			},
+		},
+	},
+});
+
+export type NotificationWithActor = Prisma.NotificationGetPayload<
+	typeof notificationWithActor
+>;
