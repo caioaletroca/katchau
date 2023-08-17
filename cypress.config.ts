66be0cd30e4cb87c2577f2651067308300e6b2fd
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-16 23:25:46 -0300

    f

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 import browserify from '@badeball/cypress-cucumber-preprocessor/browserify';
 import { defineConfig } from 'cypress';
+import cypressFailedLogs from 'cypress-failed-log/on';
 
 async function setupNodeEvents(
 	on: Cypress.PluginEvents,
@@ -16,6 +17,8 @@ async function setupNodeEvents(
 		})
 	);
 
+	cypressFailedLogs(on);
+
 	// Make sure to return the config object as it might have been modified by the plugin.
 	return config;
 }
@@ -24,6 +27,7 @@ export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:3000',
 		specPattern: '**/*.feature',
+		// reporter: require.resolve("@badeball/cypress-cucumber-preprocessor/pretty-reporter"),
 		setupNodeEvents,
 	},
 });
