05aa0d04244dc215a2b32adbaa4b2bbc49d009c9
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2023-08-19 16:48:34 -0300

    Finally

--- cypress/e2e/Chat.feature
+++ cypress/e2e/Chat.feature
@@ -1,25 +1,25 @@
-# @viewportHeight(915)
-# @viewportWidth(412)
+@viewportHeight(915)
+@viewportWidth(412)
 
-# Feature: Chat
+Feature: Chat
 	
-# 	Background:
-# 		Given I am logged in
+	Background:
+		Given I am logged in
 
-# 	@start
-# 	Scenario: Send a message
-# 		Given I navigate to Other user page
-# 		And I start a chat with Other user
-# 		When I type and send a message
-# 		Then I should see my message
+	@start
+	Scenario: Send a message
+		Given I navigate to Other user page
+		And I start a chat with Other user
+		When I type and send a message
+		Then I should see my message
 
-# 	Scenario: Search for no existing conversation
-# 		Given I navigate to Conversation page
-# 		When I search for a no existing conversation
-# 		Then I should see no results
+	Scenario: Search for no existing conversation
+		Given I navigate to Conversation page
+		When I search for a no existing conversation
+		Then I should see no results
 
-# 	@end
-# 	Scenario: Search for conversation
-# 		Given I navigate to Conversation page
-# 		When I search for Other user
-# 		Then I should be on Other user chat page
+	@end
+	Scenario: Search for conversation
+		Given I navigate to Conversation page
+		When I search for Other user
+		Then I should be on Other user chat page
--- cypress/e2e/Chat.ts
+++ cypress/e2e/Chat.ts
@@ -16,14 +16,12 @@ After({ tags: '@end' }, () => {
 });
 
 Given('I start a chat with Other user', () => {
-	getElement('profile-message').click();
-
-	cy.wait(1000);
+	getElement('profile-message').should('be.visible').click();
 });
 
 When('I type and send a message', () => {
-	getElement('chat-input').type('test');
-	getElement('chat-send').click();
+	getElement('chat-input').should('be.visible').type('test');
+	getElement('chat-send').should('be.visible').click();
 
 	cy.wait(2000);
 });
@@ -33,22 +31,29 @@ Then('I should see my message', () => {
 });
 
 When('I search for a no existing conversation', () => {
-	getElement('search-input').type('test');
+	getElement('search-input').should('be.visible').type('test');
 
 	cy.wait(1000);
 });
 
 Then('I should see no results', () => {
-	getElement('chat-conversation-results').children().should('have.length', 0);
+	getElement('chat-conversation-results')
+		.should('be.visible')
+		.children()
+		.should('have.length', 0);
 });
 
 When('I search for Other user', () => {
 	cy.fixture('users/actor').then((user) => {
-		getElement('search-input').type(user.name);
+		getElement('search-input').should('be.visible').type(user.name);
 
 		cy.wait(1000);
 
-		getElement('chat-conversation-results').children().first().click();
+		getElement('chat-conversation-results')
+			.should('be.visible')
+			.children()
+			.first()
+			.click();
 	});
 });
 
